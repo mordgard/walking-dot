14f149f87a46b246a79afdeb8613d3c040fd104f
Mikhail Sharakou <msharakov@gmail.com>
2019-10-09 20:43:54 +0300

    test

--- src/components/SideBar/Container.jsx
+++ src/components/SideBar/Container.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 import Component from "./Component";
+import { OPEN_SIDEBAR, CLOSE_SIDEBAR } from "./actions";
 
 const Container = () => {
   return <Component />;
 };
 
-export default Container;
+const mapStateToProps = state => ({
+  isSideBarOpen: state.isOpen
+});
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(
+    {
+      OPEN_SIDEBAR,
+      CLOSE_SIDEBAR
+    },
+    dispatch
+  )
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Container);
