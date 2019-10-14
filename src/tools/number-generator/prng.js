14fa323f6910284a6a3ab19d386fd3f6c84ee7a8
Alexandr Hotyan <alexhotyan@gmail.com>
2019-10-14 20:27:50 +0300

    feat: implement rnd

--- src/tools/number-generator/prng.js
+++ src/tools/number-generator/prng.js
@@ -1,5 +1,8 @@
-const pseudoRandomNumberGenerator = (max=999) => {
+const pseudoRandomNumberGenerator = (max=3) => {
   // Pseudo Random Number Generator
-};
+  Math.round(math.random()*1000);
+  
+  
+ };
 
 export default pseudoRandomNumberGenerator;
--- src/tools/number-generator/rng.js
+++ src/tools/number-generator/rng.js
@@ -1,6 +1,6 @@
 const randomNumberGenerator = (max=999) => {
   // Random Number Generator
-  
+  return (window.crypto.getRandomValues(new Uint8Array(1)));
 };
 
 export default randomNumberGenerator;
